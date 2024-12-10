05d57009806310f566a8c8575bef79ba1d991161
Re-Zero <g.rezero02@gmail.com>
2024-12-10 15:53:27 +0800

    Test

--- My_FirstProject/settings.py
+++ My_FirstProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 LOGIN_URL = 'accounts/login/'
-LOGIN_REDIRECT_URL = '/registration/admin/dashboard/'
+LOGIN_REDIRECT_URL = 'admin_dashboard'
 
 ALLOWED_HOSTS = ['registration.natcco.coop', '10.0.0.52', '10.100.1.70', 'qr-registration.onrender.com' ] #['127.0.0.1', 'localhost','*']
 
