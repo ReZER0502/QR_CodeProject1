0474c2e06b71812c8e1689654ff72ae0ffb3f65b
Re-Zero <g.rezero02@gmail.com>
2024-12-19 15:39:38 +0800

    Edited settings

--- My_FirstProject/settings.py
+++ My_FirstProject/settings.py
@@ -32,8 +32,19 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "registration",
-    "widget_tweaks"
+    "widget_tweaks",
+    "cloudinary",
+    "cloudinary_storage"
 ]
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MedicalCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'den0pbvox',
+    'API_KEY': '426942519111445',
+    'API_SECRET': '7fb60NXOsJRHPzfxsVZZ4qI4FOw'
+}
+
+MEDIA_URL = f'https://res.cloudinary.com/{CLOUDINARY_STORAGE["den0pbvox"]}/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
