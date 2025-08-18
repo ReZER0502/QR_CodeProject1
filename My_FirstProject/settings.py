67931117a02fbf548d753dfcdc42d24fc1dad8dc
Re-Zero <g.rezero02@gmail.com>
2025-08-18 16:07:14 +0800

    Update settings.py

--- My_FirstProject/settings.py
+++ My_FirstProject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-luj&v0_#m=o4whr1yh-o%#1fp$o=zf0p4*rr7*&@1y(tr#3e2^"
+SECRET_KEY = "django-insecure-luj&v0_#m=o4whrdakv(*&$^#hyrr7*&@1y(tr#3e2^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','qr.natcco.coop:6083', 'qr.natcco.coop', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 # Application definition
 AUTH_USER_MODEL = "registration.AdminUser"
@@ -66,8 +66,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Example for Gmail SMTP
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'melleattack0502@gmail.com' #qr test send 'dc@natcco.coop' 
-EMAIL_HOST_PASSWORD = 'dgva xsbv lyyn askt' #tdyt oban ueiq ahkj generated app password. hindi yung literal na password ::))) 'zahr pjxo iwgb rivx'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD =  generated app password. hindi yung literal na password ::))) 'zahr pjxo iwgb rivx'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
